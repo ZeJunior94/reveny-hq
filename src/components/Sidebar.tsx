ee7e910f747ec92b2e21a6302d311edc435579d7
ZeJunior94 <juniormarquess1994@gmail.com>
2026-06-02 19:05:44 -0300

    fix(sidebar): aumentar logo para 36px

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -47,7 +47,7 @@ export default function Sidebar() {
         <img
           src="/logo.png"
           alt="Reveny"
-          style={{ height: 22, width: 'auto', objectFit: 'contain', filter: 'brightness(0) invert(1)', opacity: 0.9 }}
+          style={{ height: 36, width: 'auto', objectFit: 'contain', filter: 'brightness(0) invert(1)', opacity: 0.9 }}
         />
       </div>
 
